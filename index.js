d67470e8030564e48e631cb2c75814ed79c03c71
Your Name <you@example.com>
2019-06-27 02:41:36 -0400

    Initial Docker Commit

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const api_url = 'https://api.primedice.com/graphql';
         }
         target = parseFloat(target/100).toFixed(2);
         let data = " mutation{primediceRoll(amount:"+amount+",target:"+target+",condition:"+ condition +",currency:"+currency+ ") { id nonce currency amount payout state { ... on CasinoGamePrimedice { result target condition } } createdAt serverSeed{seedHash seed nonce} clientSeed{seed} user{balances{available{amount currency}} statistic{game bets wins losses amount profit currency}}}}";
-        let ret = await this.doRequest('', 'POST', data, accessToken);
+        let ret = await doRequest('', 'POST', data, accessToken);
         console.log(ret);
         //let info = req.session.info;
         let betInfo = ret.primediceRoll;
