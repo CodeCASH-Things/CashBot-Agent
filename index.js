051e9d6e9b6b61942ab3d8869f37e9b8d50c7c56
Your Name <you@example.com>
2019-06-27 02:44:30 -0400

    Initial Docker Commit

--- index.js
+++ index.js
@@ -31,39 +31,8 @@ const api_url = 'https://api.primedice.com/graphql';
         }
         target = parseFloat(target/100).toFixed(2);
         let data = " mutation{primediceRoll(amount:"+amount+",target:"+target+",condition:"+ condition +",currency:"+currency+ ") { id nonce currency amount payout state { ... on CasinoGamePrimedice { result target condition } } createdAt serverSeed{seedHash seed nonce} clientSeed{seed} user{balances{available{amount currency}} statistic{game bets wins losses amount profit currency}}}}";
-        let ret = await doRequest('', 'POST', data, accessToken);
-        console.log(ret);
-        //let info = req.session.info;
-        let betInfo = ret.primediceRoll;
-        betInfo.iid = betInfo.id;
-        betInfo.condition = req.body.High == "true"?'>':'<';
-        betInfo.target = target;
-        betInfo.profit = (parseFloat(betInfo.payout) - parseFloat(betInfo.amount)).toFixed(8);
-        betInfo.roll = parseFloat(betInfo.state.result).toFixed(2);
-        betInfo.payout = parseFloat(betInfo.payout).toFixed(8);
-        betInfo.amount = parseFloat(betInfo.amount).toFixed(8);
-        info.info.balance = (parseFloat(info.info.balance) + parseFloat(betInfo.profit)).toFixed(8);
-        info.currentInfo.balance = (parseFloat(info.currentInfo.balance) + parseFloat(betInfo.profit)).toFixed(8);
-        info.info.bets++;
-        info.currentInfo.bets++;
-        info.info.profit = (parseFloat(info.info.profit) + parseFloat(betInfo.profit)).toFixed(8);
-        info.info.wagered = (parseFloat(info.info.wagered) + parseFloat(amount)).toFixed(8);
-        info.currentInfo.wagered = (parseFloat(info.currentInfo.wagered) + parseFloat(amount)).toFixed(8);
-        info.currentInfo.profit = (parseFloat(info.currentInfo.profit) + parseFloat(betInfo.profit)).toFixed(8);
-        if(betInfo.profit>0){
-            betInfo.win = true;
-            info.info.wins++;
-            info.currentInfo.wins++;
-        } else {
-            betInfo.win = false;
-            info.info.losses++;
-            info.currentInfo.losses++;
-        }
-        let returnInfo = {};
-        returnInfo.betInfo= betInfo;
-        returnInfo.info = info;
-        //req.session.info = info;
-        return returnInfo;
+        let ret = await doRequest('', 'POST', data, accessToken);       
+        return ret;
     }
 
    async function doRequest(route, method, body, accessToken){
