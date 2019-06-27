ead924ff7bfe04dedc1dae72a583c07c856e7dd7
Your Name <you@example.com>
2019-06-27 01:39:42 -0400

    Initial Docker Commit

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Checking REPO URL: $REPO_URL"
+echo "Verifying Path: $APP_PATH"
+echo "Verifying Script Path: $SCRIPT_PATH"
+if [ ! -z "$APP_REPO_URL" ]; then
+	if [ ! -d "$APP_PATH/src" ]; then
+		echo "SRC Dir Missing, Cloning Repos."
+		git clone "$APP_REPO_URL" "$APP_PATH"
+		git clone "$SCRIPT_REPO_URL" "$SCRIPT_PATH"
+		cd "$APP_PATH/src"
+		npm install
+	fi
+fi	
+
+ln -s "$SCRIPT_PATH" "/usr/local/bin/script"
+
+
+cd "$APP_PATH"
+exec "$@"
+
